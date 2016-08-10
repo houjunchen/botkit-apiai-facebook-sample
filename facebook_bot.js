2d0bddb3c004471c613e573813cc71194bf2135d
Abner Chen <abner@savy.tw>
2016-08-10 10:33:31 +0800

    Add facebook_bot.js

---
+++ facebook_bot.js
@@ -0,0 +1,119 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
+This is a sample Facebook bot built with Botkit.
+
+This bot demonstrates many of the core features of Botkit:
+
+* Connect to Facebook's Messenger APIs
+* Receive messages based on "spoken" patterns
+* Reply to messages
+* Use the conversation system to ask questions
+* Use the built in storage system to store and retrieve information
+  for a user.
+
+# RUN THE BOT:
+
+  Follow the instructions here to set up your Facebook app and page:
+
+    -> https://developers.facebook.com/docs/messenger-platform/implementation
+
+  Run your bot from the command line:
+
+    page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js [--lt [--ltsubdomain LOCALTUNNEL_SUBDOMAIN]]
+
+  Use the --lt option to make your bot available on the web through localtunnel.me.
+
+# EXTEND THE BOT:
+
+  Botkit has many features for building cool and useful bots!
+
+  Read all about it here:
+
+    -> http://howdy.ai/botkit
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+if (!process.env.page_token) {
+    console.log('Error: Specify page_token in environment');
+    process.exit(1);
+}
+
+if (!process.env.verify_token) {
+    console.log('Error: Specify verify_token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('botkit');
+var os = require('os');
+var commandLineArgs = require('command-line-args');
+var localtunnel = require('localtunnel');
+var apiai = require('apiai');
+
+const cli = commandLineArgs([
+      {name: 'lt', alias: 'l', args: 1, description: 'Use localtunnel.me to make your bot available on the web.',
+      type: Boolean, defaultValue: false},
+      {name: 'ltsubdomain', alias: 's', args: 1,
+      description: 'Custom subdomain for the localtunnel.me URL. This option can only be used together with --lt.',
+      type: String, defaultValue: null},
+   ]);
+
+const ops = cli.parse();
+if(ops.lt === false && ops.ltsubdomain !== null) {
+    console.log("error: --ltsubdomain can only be used together with --lt.");
+    process.exit();
+}
+
+var controller = Botkit.facebookbot({
+    debug: false,
+    access_token: process.env.page_token,
+    verify_token: process.env.verify_token,
+});
+
+var apiaibot = apiai(process.env.dashbot_key);
+var dashbot = require('dashbot')(process.env.apiai_token).facebook;
+
+controller.middleware.receive.use(dashbot.receive);
+controller.middleware.send.use(dashbot.send);
+
+var bot = controller.spawn({
+});
+
+controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        console.log('ONLINE!');
+        if(ops.lt) {
+            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+                if (err) {
+                    console.log(err);
+                    process.exit();
+                }
+                console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
+            });
+
+            tunnel.on('close', function() {
+                console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
+                process.exit();
+            });
+        }
+    });
+});
+
+controller.on('message_received', function(bot, message) {
+    console.log(message);
+    var request = apiaibot.textRequest(message.text);
+    request.on('response', function(response) {
+        bot.reply(message, response.result.fulfillment.speech);
+    });
+    request.on('error', function(error) {
+        console.log(error);
+        bot.reply(message, error);
+    });
+    request.end();
+});
